c1f1bec07ee41fabf5a5ca1c2bbea35f02eb7ddf
shivamgarg1 <shivamgarg1445@gmail.com>
2023-08-11 00:04:33 -0500

    Time: 1715 ms (86.57%), Space: 55 MB (93.61%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -4,10 +4,8 @@ class Solution:
         primes = [True] * n
         primes[0] = False
         primes[1] = False
-        count = 0
         for i in range(2, int(sqrt(n)) + 1):
             if primes[i]:
-                count += 1
                 for multiple in range(2*i, n, i):
                     primes[multiple] = False
         return sum(primes)
