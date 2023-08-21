1757e63310bc0de4a39f9d6796f1436bfbb18960
shivamgarg1 <shivamgarg1445@gmail.com>
2023-08-21 17:03:16 -0500

    Time: 180 ms (99.00%), Space: 26 MB (14.66%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,32 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        '''
+        
+        1)a mappingform email address to account nanme
+        2) form a graph 
+        
+        
+        '''
+        
+        alist = defaultdict(list)
+        for i in range(len(accounts)):
+            for account in accounts[i][2:]:
+                alist[accounts[i][1]].append(account)
+                alist[account].append(accounts[i][1])
+        visited = set()
+        
+        def dfs(node, stack):
+            if node in visited:return
+            stack.append(node)
+            visited.add(node)
+            for nei in alist[node]:
+                dfs(nei, stack)
+        
+        res = []  
+        for i in range(len(accounts)):
+            local_res = []
+            for account in accounts[i][1:]:
+                dfs(account, local_res)
+            if local_res:
+                res.append([accounts[i][0]] + sorted(local_res))
+        return res
