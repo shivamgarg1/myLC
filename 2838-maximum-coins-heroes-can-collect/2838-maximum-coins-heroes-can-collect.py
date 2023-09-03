943b9650dac27cdd2aebb9acefb766e0904f8650
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-03 15:23:46 -0500

    Time: 908 ms (8.33%), Space: 38.5 MB (76.39%) - LeetHub

--- 2838-maximum-coins-heroes-can-collect/2838-maximum-coins-heroes-can-collect.py
+++ 2838-maximum-coins-heroes-can-collect/2838-maximum-coins-heroes-can-collect.py
@@ -8,29 +8,16 @@ class Solution:
         prefix_sum - [2, 5, 10, 16, 20]
         '''
         
-        coin_l = len(coins)
-        monst_coin = [(monsters[i], coins[i]) for i in range(coin_l)]
-        monst_coin = sorted(monst_coin, key = lambda x:x[0])
-        prefix_sum = [monst_coin[0][1]]
-        for i in range(1, coin_l):
-            prefix_sum.append(prefix_sum[i-1] + monst_coin[i][1])
-        res = []
+        mon_cost = sorted(list(zip(monsters, coins)))
+        ps = [0]
+        for _, c in mon_cost: ps.append(ps[-1] + c)
 
-        def binary_search(tgt):
-            l = 0 
-            r = coin_l
-            while l < r:
-                mid = l + ( r - l ) // 2
-                if monst_coin[mid][0] > tgt:
-                    r = mid
-                else:
-                    l = mid + 1
-            
-            return l
-        
-        for hero in heroes:
-            l = binary_search(hero)
-            if l == 0 and monst_coin[0][0] > hero:
-                res.append(0)
-            else: res.append(prefix_sum[l - 1])
+        res = []
+        for h in heroes:
+            l, r = 0, len(monsters) - 1
+            while l <= r:
+                m = (l + r) // 2
+                if mon_cost[m][0] > h: r = m - 1
+                else: l = m + 1
+            res.append(ps[l])
         return res
