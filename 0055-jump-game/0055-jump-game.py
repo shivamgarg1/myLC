094b0fe67f5ee976b345e4f743bcd515e57d5bd4
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-08 22:48:36 -0500

    Time: 7550 ms (5.00%), Space: 31.6 MB (5.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,14 +2,17 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         
         l = len(nums)
-        
-        @lru_cache(maxsize=None)
+        dp = {}
+        #@lru_cache(maxsize=None)
         def rec(i):
             if i >= l-1:return True
             if nums[i] == 0: return False
-            
+            if i in dp:return dp[i]
             for val in range(nums[i], 0, -1):
-                if rec(i + val):return True
+                if rec(i + val):
+                    dp[i] = True
+                    return True
+            dp[i] = False
             return False
         
         return rec(0)
