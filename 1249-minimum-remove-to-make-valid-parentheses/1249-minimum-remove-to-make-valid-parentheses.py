31bea92fb33960cca783aa6889058c05f0954d50
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-16 15:35:30 -0500

    Time: 120 ms (26.78%), Space: 17.6 MB (93.94%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,45 +1,33 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         '''
-        )(
-        opens/ closes
-        (()
-        ())
-        
+        ((
         '''
         
         opens = 0
-        res = ''
+        new_s = ''
         for char in s:
-            if char == '(':
+            if char == ')' and not opens:continue
+            elif char == '(':
                 opens += 1
-                res += char
-            elif char == ')' and opens > 0:
-                opens -=1 
-                res += char
-            elif char == ')' and opens == 0:
-                continue
-            else:
-                res += char
-        ans = ''
+            elif char == ')':
+                opens -= 1
+            new_s += char
         
-        if opens > 0:
-            l = len(res)
-            i = l - 1
-            closes = 0
-            while i >= 0:
-                if res[i] == ')':
-                    closes += 1
-                    ans += res[i]
-                elif res[i] == '(' and closes > 0:
-                    closes -= 1
-                    ans += res[i]
-                elif res[i] == '(' and closes == 0:
-                    i -= 1
-                    continue
-                else:
-                    ans += res[i]
-                i -= 1 
+        if opens == 0:return new_s
+        
+        closes = 0
+        res = ''
+        for i in range(len(new_s) - 1, -1, -1):
+            if new_s[i] == ')':
+                res += new_s[i]
+                closes += 1
+            elif new_s[i] == '(' and not closes: continue
+            elif new_s[i] == '(':
+                closes -= 1
+                res += new_s[i]
+            else:
+                res += new_s[i]
+        
+        return res[::-1]
             
-            return ans[::-1]
-        else: return res
