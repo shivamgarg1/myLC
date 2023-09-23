c56e4ae88b6fb2f888774b2954acc4ccbe1b401b
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-23 18:20:23 -0500

    Time: 3343 ms (5.00%), Space: 468.4 MB (5.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,17 +3,20 @@ class Solution:
         
         l = len(coins)
         @lru_cache(maxsize=None)
-        def rec(rs):
-            if rs > amount:return -1
-            elif rs == amount:return 0
+        def rec(i, rs):
+            if i == l:
+                if rs > amount:return -1
+                elif rs == amount:return 0
+            elif rs > amount:return -1
+            elif rs == amount: return 0
             
             tmp = sys.maxsize
-            for coin in coins:
-                if coin + rs <= amount:
-                    count = rec(rs + coin)
+            for j in range(i, l):
+                if coins[j] + rs <= amount:
+                    count = rec(j, rs + coins[j])
                     if count != -1:
                         tmp = min(tmp, count + 1)
             
             return tmp if tmp != sys.maxsize else -1
         
-        return rec(0)
+        return rec(0, 0)
