ecab99bdb452fce45661d6e7f4f3b5b9782ea320
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-23 18:18:05 -0500

    Time: 1077 ms (35.56%), Space: 36.5 MB (16.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        dp = {}
-        
-        #@lru_cache(maxsize=None)
-        def rec(amt):
-            if amt in dp:return dp[amt]
-            if amt == 0:return 0
-            min_count = sys.maxsize
+        l = len(coins)
+        @lru_cache(maxsize=None)
+        def rec(rs):
+            if rs > amount:return -1
+            elif rs == amount:return 0
+            
+            tmp = sys.maxsize
             for coin in coins:
-                if amt >= coin:
-                    count = rec(amt - coin)
-                    min_count = min(min_count, count)
+                if coin + rs <= amount:
+                    count = rec(rs + coin)
+                    if count != -1:
+                        tmp = min(tmp, count + 1)
             
-            dp[amt] = min_count + 1
-            return min_count + 1
+            return tmp if tmp != sys.maxsize else -1
         
-        count = rec(amount)
-        if count >= sys.maxsize:return -1
-        return count
-            
-        
+        return rec(0)
