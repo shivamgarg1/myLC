68b942fbdd3e27532dfd2f15df9bb24e2a98b2b3
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-23 17:30:03 -0500

    Time: 37 ms (66.74%), Space: 16.4 MB (26.37%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -2,19 +2,17 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         
         m = defaultdict(int)
-        for i, char in enumerate(order):
-            m[char] = i
+        for char in s:
+            m[char] += 1
         
-        def compare(left, right):
-            if left in m and right in m:
-                if m[left] < m[right]:return -1
-                elif m[left] > m[right]: return 1
-                else:return 0
-            elif left in m: return -1
-            elif right in m:  
-                return 1
-            else:return 0
+        res = ''
+        for char in order:
+            if char in m:
+                res += char * m[char]
+                m[char] = 0
         
-        res = list(s)
-        res.sort(key=cmp_to_key(compare))
+        for char in s:
+            if m[char] != 0:
+                res += m[char] * char
+                m[char] = 0
         return ''.join(res)
