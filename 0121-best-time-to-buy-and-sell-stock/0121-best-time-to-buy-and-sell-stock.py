5dc0c43e346fa80e8ed8eb06452e6e11d32574fe
shivamgarg1 <shivamgarg1445@gmail.com>
2023-09-23 20:09:59 -0500

    Time: 839 ms (71.28%), Space: 27.4 MB (13.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -8,16 +8,16 @@ class Solution:
         
         
         '''
+        
         low = prices[0]
-        high = prices [0]
-        max_profit = 0
+        high = prices[0]
+        max_profit = -sys.maxsize
         for price in prices:
             if price > high:
                 high = price
             elif price < low:
                 low = price
                 high = price
-            
-            max_profit = max(high - low, max_profit)
+            max_profit = max(max_profit, high - low)
         
-        return max_profit
+        return max_profit if max_profit > 0 else 0
