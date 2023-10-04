fa778c27ab23193dcd463200bc84de1849be7eba
shivamgarg1 <shivamgarg1445@gmail.com>
2023-10-04 01:07:30 -0500

    Time: 95 ms (55.94%), Space: 21.1 MB (19.93%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,18 +1,37 @@
-class Solution {
-public:
-    string removeDuplicates(string s, int k) {
-        vector<pair<int, char>> counts;
-    for (int i = 0; i < s.size(); ++i) {
-        if (counts.empty() || s[i] != counts.back().second) {
-            counts.push_back({ 1, s[i] });
-        } else if (++counts.back().first == k) {
-            counts.pop_back();
-        }
-    }
-    s = "";
-    for (auto &p : counts) {
-        s += string(p.first, p.second);
-    }
-    return s;
-    }
-};
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        '''   
+        abbcccb -> a
+        algo:
+        1) initialize a stack
+        2) iterate string and keep count until elements are same
+        3) if different, insert char and count
+        4) when count == k, dont store anything in the satck and continue iterating
+        5) else check top and remove if needed
+        
+        abccb
+        (a, 1), (b, 1)  
+         0 
+         
+         a b c c b d
+         
+         stack = [['#', 0]]
+        for c in s:
+            if stack[-1][0] == c:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([c, 1])
+        return ''.join(c * k for c, k in stack)
+        '''
+        stack = [['', 0]]
+        for char in s:
+            if stack[-1][0] == char:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([char, 1])
+        
+        return ''.join([char * count for char, count in stack])
