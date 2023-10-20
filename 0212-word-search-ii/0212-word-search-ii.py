140a5b1d61fc9a0a9b8bbf0837b62755a799de32
shivamgarg1 <shivamgarg1445@gmail.com>
2023-10-20 00:18:16 -0500

    Time: 6461 ms (47.38%), Space: 19.3 MB (42.46%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -15,26 +15,23 @@ class Solution:
             node['$'] = word
         
         
-        def dfs(r, c, stack, node):
-            if (r, c) in stack:return False
+        def dfs(r, c, node):
             if board[r][c] not in node:return False
             char = board[r][c]
             if '$' in node[char]:
                 self.res.append(node[char]['$'])
                 node[char].pop('$')
-            stack.add((r, c))
-            
+            board[r][c] = '#'
             for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
                 new_r = r + dr
                 new_c = c + dc
                 if 0 <= new_r < R and 0 <= new_c < C:
-                    dfs(new_r, new_c, stack, node[char])
-            
-            stack.remove((r, c))
+                    dfs(new_r, new_c, node[char])
+            board[r][c] = char
     
         for r in range(R):
             for c in range(C):
-                dfs(r, c, set(), self.trie)
+                dfs(r, c, self.trie)
         
         return self.res
             
